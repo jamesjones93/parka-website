4ad050aeca6073e2d6a5d49d0cd9fea10d52476c
James Jones <james jones>
2018-05-14 11:53:58 +0100

    deploy

--- src/SignUp.js
+++ src/SignUp.js
@@ -240,7 +240,7 @@ const HereLink = styled.span`
 const ErrorMessage = styled.p`
     color: white;
     text-align: center;
-    width: 370px;
+    width: 350px;
     margin: 0 auto;
 
     @media only screen and (max-device-width: 768px) {
